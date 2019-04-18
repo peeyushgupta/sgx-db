9e754cfd1f12f4b00aa2f1512496086157d9a0b2
Tianjiao Huang <maitian519@gmail.com>
2019-04-18 16:39:24 -0700

    refactor out the per-dblk-lvl-"fill_bins" function

--- enclave/bin_packing_join/bin_packing_join.cpp
+++ enclave/bin_packing_join/bin_packing_join.cpp
@@ -164,122 +164,11 @@ int fill_bins(data_base_t *db, table_t *data_table, int column,
 #if defined(REPORT_BIN_PACKING_JOIN_STATS)
     INFO("Reading bin info table from row %d to %d\n", start_row, end_row);
 #endif
-#if !defined(NDEBUG)
-    bool data_table_exhausted = false;
-#endif
     // For each datablock
-    for (int row_num = start_row; row_num < end_row; ++dblk_cnt) {
-        // Load bin information
-        // This map stores the information about a value should be placed in
-        // which cell.
-        std::unordered_map<std::string, int> placement;
-        int byte_read_info = 0;
-        // For each cell
-        for (int cell_num = 0; cell_num < num_bins; ++cell_num) {
-            // For each value
-            for (int i = 0; i < rows_per_cell; ++i) {
-                row_t row;
-#if !defined(NDEBUG)
-                if (row_num >= bin_info_table->num_rows) {
-                    ERR("Reach the end of data table but still have more rows "
-                        "to read\n");
-                    return -1;
-                }
-#endif
-
-                if (read_row(bin_info_table, row_num++, &row)) {
-                    ERR("Failed to read row");
-                    return -1;
-                }
-
-                std::string value(
-                    (char *)get_column(&(bin_info_table->sc), 0, &row));
-                if (value.empty()) {
-                    continue;
-                }
-
-                byte_read_info += value.length();
-#if !defined(NDEBUG)
-                const auto &it = placement.find(value);
-                if (it != placement.end()) {
-                    ERR("Value \"%s\" is appeared in the datablock %d twice at "
-                        "cell %d row %d\n",
-                        value.c_str(), dblk_cnt, cell_num, i);
-                    return -1;
-                }
-#endif
-                placement[value] = cell_num;
-            }
-        }
-
-        // Scan the data table and fill bin according to the bin information
-        int byte_read_bin = 0;
-        typedef std::vector<row_t> temp_bin_t;
-        std::vector<temp_bin_t> temp_bins(num_bins);
-        for (int i = 0; i < rows_per_dblk; ++i) {
-            row_t row;
-            if (data_row_num >= data_table->num_rows) {
-#if !defined(NDEBUG)
-                if (data_table_exhausted) {
-                    ERR("Reach the end of data table but still have more rows "
-                        "to read. Data table: dblk %d, curr_row %d, "
-                        "rows_per_dblk. Info table: total rows %d, curr_row "
-                        "%d %d\n",
-                        dblk_cnt, i, rows_per_dblk, bin_info_table->num_rows,
-                        row_num, 1);
-                    return -1;
-                }
-                data_table_exhausted = true;
-#endif
-                break;
-            }
-
-            if (read_row(data_table, data_row_num++, &row)) {
-                ERR("Failed to read row");
-                return -1;
-            }
-            std::string value(
-                (char *)get_column(&(data_table->sc), column, &row));
-#if !defined(NDEBUG)
-            const auto it = placement.find(value);
-            if (it == placement.end()) {
-                ERR("Failed to find bin info for '%s'\n", value.c_str());
-                return -1;
-            }
-            if (it->second < 0 || it->second >= temp_bins.size()) {
-                ERR("Bin index out of bound: %d\n", it->second);
-                return -1;
-            }
-#endif
-            const auto &key = placement[value];
-            auto &bin = temp_bins[key];
-            try {
-                bin.push_back(row);
-                byte_read_bin += sizeof(row);
-            } catch (const std::bad_alloc &) {
-                ERR("Not enough memory: dblk %d, i %d, row_num %d, byte_info "
-                    "%d, byte_bin %d\n",
-                    dblk_cnt, i, data_row_num, byte_read_info, byte_read_bin);
-                return -1;
-            }
-        }
-
-        // Flush temp_bins to bins
-        row_t empty_row;
-        memset(&empty_row, 0x0, sizeof(empty_row));
-        empty_row.header.fake = true;
-        for (int i = 0; i < temp_bins.size(); ++i) {
-            temp_bin_t *temp_bin = &temp_bins[i];
-            for (int j = 0; j < rows_per_cell; j++) {
-                row_t *row;
-                if (j < temp_bin->size()) {
-                    row = &(*temp_bin)[j];
-                } else {
-                    row = &empty_row;
-                }
-                insert_row_dbg((*bins)[i], row);
-            }
-        }
+    for (int info_row_num = start_row; info_row_num < end_row; ++dblk_cnt) {
+        fill_bins_per_dblk(data_table, column, &data_row_num, rows_per_dblk,
+                           dblk_cnt, bin_info_table, &info_row_num,
+                           rows_per_cell, bin_sc, bins);
     }
 
     return 0;
@@ -338,6 +227,124 @@ int join_bins(table_t *lhs_tbl, const int lhs_column, table_t *rhs_tbl,
     memset(&empty_row, 0x0, sizeof(empty_row));
     for (; num_rows_inserted < num_rows_per_out_bin; ++num_rows_inserted) {
         insert_row_dbg(join_tbl, &empty_row);
+    }
+
+    return 0;
+}
+
+int fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
+                       const int rows_per_dblk, const int dblk_cnt,
+                       table_t *bin_info_table, int *info_row_num,
+                       const int rows_per_cell, schema_t *bin_sc,
+                       std::vector<table_t *> *bins) {
+    // Load bin information
+    // This map stores the information about a value should be placed in
+    // which cell.
+    const int num_bins = bins->size();
+    std::unordered_map<std::string, int> placement;
+    int byte_read_info = 0;
+    // For each cell
+    for (int cell_num = 0; cell_num < num_bins; ++cell_num) {
+        // For each value
+        for (int i = 0; i < rows_per_cell; ++i) {
+            row_t row;
+#if !defined(NDEBUG)
+            if (*info_row_num >= bin_info_table->num_rows) {
+                ERR("Reach the end of data table but still have more rows "
+                    "to read\n");
+                return -1;
+            }
+#endif
+
+            if (read_row(bin_info_table, (*info_row_num)++, &row)) {
+                ERR("Failed to read row");
+                return -1;
+            }
+
+            std::string value(
+                (char *)get_column(&(bin_info_table->sc), 0, &row));
+            if (value.empty()) {
+                continue;
+            }
+
+            byte_read_info += value.length();
+#if !defined(NDEBUG)
+            const auto &it = placement.find(value);
+            if (it != placement.end()) {
+                ERR("Value \"%s\" is appeared in the datablock %d twice at "
+                    "cell %d row %d\n",
+                    value.c_str(), dblk_cnt, cell_num, i);
+                return -1;
+            }
+#endif
+            placement[value] = cell_num;
+        }
+    }
+
+    // Scan the data table and fill bin according to the bin information
+    int byte_read_bin = 0;
+    typedef std::vector<row_t> temp_bin_t;
+    std::vector<temp_bin_t> temp_bins(num_bins);
+    for (int i = 0; i < rows_per_dblk; ++i) {
+        row_t row;
+        if (*data_row_num >= data_table->num_rows) {
+            // TODO: make sure that there's no more info_bin to read in the
+            // outer loop if (data_table_exhausted) {
+            //     ERR("Reach the end of data table but still have more rows "
+            //         "to read. Data table: dblk %d, curr_row %d, "
+            //         "rows_per_dblk. Info table: total rows %d, curr_row "
+            //         "%d %d\n",
+            //         dblk_cnt, i, rows_per_dblk, bin_info_table->num_rows,
+            //         row_num, 1);
+            //     return -1;
+            // }
+            break;
+        }
+
+        if (read_row(data_table, (*data_row_num)++, &row)) {
+            ERR("Failed to read row");
+            return -1;
+        }
+        std::string value((char *)get_column(&(data_table->sc), column, &row));
+#if !defined(NDEBUG)
+        const auto it = placement.find(value);
+        if (it == placement.end()) {
+            ERR("Failed to find bin info for '%s'\n", value.c_str());
+            return -1;
+        }
+        if (it->second < 0 || it->second >= temp_bins.size()) {
+            ERR("Bin index out of bound: %d\n", it->second);
+            return -1;
+        }
+#endif
+        const auto &key = placement[value];
+        auto &bin = temp_bins[key];
+        try {
+            bin.push_back(row);
+            byte_read_bin += sizeof(row);
+        } catch (const std::bad_alloc &) {
+            ERR("Not enough memory: dblk %d, i %d, row_num %d, byte_info "
+                "%d, byte_bin %d\n",
+                dblk_cnt, i, data_row_num, byte_read_info, byte_read_bin);
+            return -1;
+        }
+    }
+
+    // Flush temp_bins to bins
+    row_t empty_row;
+    memset(&empty_row, 0x0, sizeof(empty_row));
+    empty_row.header.fake = true;
+    for (int i = 0; i < temp_bins.size(); ++i) {
+        temp_bin_t *temp_bin = &temp_bins[i];
+        for (int j = 0; j < rows_per_cell; j++) {
+            row_t *row;
+            if (j < temp_bin->size()) {
+                row = &(*temp_bin)[j];
+            } else {
+                row = &empty_row;
+            }
+            insert_row_dbg((*bins)[i], row);
+        }
     }
 
     return 0;
--- enclave/bin_packing_join/bin_packing_join.hpp
+++ enclave/bin_packing_join/bin_packing_join.hpp
@@ -11,12 +11,14 @@
 // `data_table`: the table that stores the actual data.
 // `rows_per_dblk`: number of rows per datablock in `data_table`.
 // `bin_info_table`: the table that stores the bin information that we
-// collection in Phase 2. `start_dblk`: the first datablock we need to read in
+//      collection in Phase 2. `start_dblk`: the first datablock we need to read
+//      in
 // `bin_info_table` `end_dblk`: the last datablock we need to read in
 // `bin_info_table` `num_bins`: number of bins. `num_rows_per_bins`: number of
-// rows in a bin in the `bin_info_table`. `bin_sc`: The schema of the bins. We
-// use this to create the actual bins. `column_offset`: Where the two tables are
-// sperated in the `bin_sc`. `bins`: The actual bins, which is the output of
+//      rows in a bin in the `bin_info_table`. `bin_sc`: The schema of the bins.
+//      We use this to create the actual bins. `column_offset`: Where the two
+//      tables are sperated in the `bin_sc`.
+// `bins`: The actual bins, which is the output of
 // Phase 3 and the input of Phase 4.
 // TODO: parallelize this
 // TODO: get `bin_sc` from table
@@ -26,11 +28,14 @@ int fill_bins(data_base_t *db, table_t *data_table, int column,
               const int rows_per_cell, schema_t *bin_sc,
               std::vector<table_t *> *bins);
 // Helper for fill_bins
-// Fill one bin. The information of the bin is located at
-// bin_info_table[begin:end)
-int fill_bin(table_t *bin_info_table, int begin, int end, int num_bins,
-             int num_rows_per_bin, table_t *bin);
-
+// Fill bin in a datablock. The information of the bin is located at
+// `row_num`: to keep track of which row are we on in the `data_table.
+//  Increment as we scan through the `data_table`.
+int fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
+                       const int rows_per_dblk, const int dblk_cnt,
+                       table_t *bin_info_table, int *info_row_num,
+                       const int rows_per_cell, schema_t *bin_sc,
+                       std::vector<table_t *> *bins);
 // Bin Packing Phase 4: join each individual bin
 // For each individual bin, do a hash join, do padding(while pretending not),
 // and write the results out
