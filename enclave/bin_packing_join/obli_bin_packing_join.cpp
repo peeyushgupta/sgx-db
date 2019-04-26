13d2d82ab5dc08453f37dbacb83e1f112b325607
Tianjiao Huang <maitian519@gmail.com>
2019-04-26 12:46:14 -0700

    pad to pow_2

--- enclave/bin_packing_join/obli_bin_packing_join.cpp
+++ enclave/bin_packing_join/obli_bin_packing_join.cpp
@@ -17,6 +17,8 @@
 #include "enclave_t.h"
 #endif
 
+int nearest_pow_2(int x) { return 1 << (sizeof(x) * 8 - __builtin_clz(x - 1)); }
+
 int obliv_fill_bins(data_base_t *db, table_t *data_table, int column,
                     const int rows_per_dblk, table_t *bin_info_table,
                     const int start_dblk, const int end_dblk,
@@ -32,6 +34,7 @@ int obli_fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
                             table_t *bin_info_table, int *info_row_num,
                             const int rows_per_cell, schema_t *bin_sc,
                             std::vector<table_t *> *bins) {
+    int rtn = 123456;
     // Create a backup for data_row_num since we will use it multiple times
     const int initial_data_row_num = *data_row_num;
     // Load bin information
@@ -42,6 +45,7 @@ int obli_fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
     // For each cell
     for (int cell_num = 0; cell_num < num_bins; ++cell_num) {
         DBG("filling dblk #%d, cell#%d\n", dblk_cnt, cell_num);
+        table_t *bin = bins->at(cell_num);
         std::vector<std::string> keys;
         // Load each value in `bin_info_table` into `keys`
         for (int i = 0; i < rows_per_cell; ++i) {
@@ -75,7 +79,16 @@ int obli_fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
             }
         }
 
-        // Scan the entire datablock(expensive) and load data
+        // Create temp_bin
+        table_t *temp_bin;
+        std::string temp_bin_name = bin->name + "_obli_phase3_temp_bin";
+        rtn = create_table(bin->db, temp_bin_name, &bin->sc, &temp_bin);
+        if (rtn) {
+            ERR("Failed to create temp_bin");
+            return -7;
+        }
+
+        // Scan the entire datablock(expensive) and load data into temp_bin
         *data_row_num = initial_data_row_num;
         int initial_bin_row_num = (*bins)[cell_num]->num_rows;
         for (int i = 0; i < rows_per_dblk; ++i) {
@@ -101,16 +114,47 @@ int obli_fill_bins_per_dblk(table_t *data_table, int column, int *data_row_num,
             row_t fake_row;
             memset(&fake_row, 0x0, sizeof(fake_row));
             fake_row.header.fake = true;
-            // TODO: add this back
+            // TODO: add the oblivious code bad
+            if (should_not_load) {
+                memcpy(&data_row, &fake_row, sizeof(data_row));
+            }
+
             // obli_cswap((u8*)&data_row, (u8*)&fake_row, sizeof(row_t),
             // should_not_load);
 
-            insert_row_dbg(bins->at(cell_num), &data_row);
+            insert_row_dbg(temp_bin, &data_row);
         }
 
-        recBitonicSort(bins->at(cell_num), initial_bin_row_num,
-                       bins->at(cell_num)->num_rows - initial_bin_row_num,
-                       column, 1, 0);
+        DBG("Keys: \n");
+        for (const auto& key : keys) {
+            DBG("%s\n", key.c_str());
+        }
+        DBG("\n");
+
+        // Append fake row to make
+        row_t fake_row;
+        memset(&fake_row, 0x0, sizeof(fake_row));
+        fake_row.header.fake = true;
+        int n = nearest_pow_2(temp_bin->num_rows);
+        assert(__builtin_popcount(n) == 1);
+        for (int i = temp_bin->num_rows; i < n; ++i) {
+            insert_row_dbg(temp_bin, &fake_row);
+        }
+        // print_table_dbg(temp_bin, 0, 100);
+
+        // Sort temp bin
+        table_t *sorted_temp_bin = nullptr;
+        bitonic_sort_table(temp_bin->db, temp_bin, column, &sorted_temp_bin);
+#if !defined(NDEBUG)
+        if (sorted_temp_bin != nullptr) {
+            ERR("A sorted_temp_bin is created unexpectedly.\n");
+            return -1;
+        }
+#endif
+        print_table_dbg(temp_bin, 8000, 8100);
+        delete_table(temp_bin->db, temp_bin);
+        assert(false);
+        return -1;
     }
 
     *data_row_num = initial_data_row_num + rows_per_dblk;
